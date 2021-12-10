673c55694218e97f4908a3aff745a06d0f3182ad
Arlo <arloe.jones3@gmail.com>
2021-12-09 20:52:06 -0800

    Intital commit

---
+++ Cpts321 Final Exam/Cpts321 Final Exam/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cpts321_Final_Exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
