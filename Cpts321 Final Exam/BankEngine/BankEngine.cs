237812fd1a6349999e14401d97a43ac245b6c5e3
Arlo <arloe.jones3@gmail.com>
2021-12-09 23:33:10 -0800

    Wrote some skeleton code for classes and functions in BankEngine

---
+++ Cpts321 Final Exam/BankEngine/BankEngine.cs
@@ -0,0 +1,44 @@
+﻿// <copyright file="BankEngine.cs" company="Arlo Jones">
+// Copyright (c) { Aejace studios }. All rights reserved.
+// </copyright>
+
+namespace Bank_Engine
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Manages users, bank accounts, and transactions. Used to inform UI.
+    /// </summary>
+    public class BankEngine
+    {
+        public void AddUser()
+        {
+
+        }
+
+        public void AddAccount()
+        {
+
+        }
+
+        /// <summary>
+        /// .
+        /// </summary>
+        private class Client : User
+        {
+
+        }
+
+        /// <summary>
+        /// .
+        /// </summary>
+        private class Administrator : User
+        {
+
+        }
+    }
+}
--- Cpts321 Final Exam/Cpts321 Final Exam/Program.cs
+++ Cpts321 Final Exam/Cpts321 Final Exam/Program.cs
@@ -5,6 +5,7 @@
 namespace Cpts321_Final_Exam
 {
     using System;
+    using Bank_Engine;
 
     /// <summary>
     /// Class to contain function "Main".
@@ -17,7 +18,23 @@ namespace Cpts321_Final_Exam
         /// <param name="args"> Arguments passed into main (if any). </param>
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Declare Local variables
+            var bankEngine = new BankEngine();
+
+            // Initialize some history for the bank engine
+            bankEngine.AddUser();
+
+            // Home menu
+
+
+
+
+
+
+
+
+
+            //Console.WriteLine("Hello World!");
         }
     }
 }
