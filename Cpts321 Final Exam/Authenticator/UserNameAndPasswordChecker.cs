976c1583a79041c91e8b8c91ea9c20ca60731290
Arlo <arloe.jones3@gmail.com>
2021-12-10 20:48:50 -0800

    Wrote authenticate function for the authenticator library.

--- Cpts321 Final Exam/Authenticator/UserNameAndPasswordChecker.cs
+++ Cpts321 Final Exam/Authenticator/UserNameAndPasswordChecker.cs
@@ -2,6 +2,8 @@
 // Copyright (c) { Aejace studios }. All rights reserved.
 // </copyright>
 
+using System.Collections.Generic;
+
 namespace Authenticator
 {
     using System;
@@ -11,19 +13,34 @@ namespace Authenticator
     /// </summary>
     public class UserNameAndPasswordChecker
     {
-        // Dictionary of UserName Keys and password values
-        // Constructor populates dictionary by reading in from a file
-        // Authenticate function that checks and returns a boolean
+        /// <summary>
+        /// A dictionary containing valid username and password combinations.
+        /// </summary>
+        private Dictionary<string, string> validUsers;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserNameAndPasswordChecker"/> class.
+        /// </summary>
+        public UserNameAndPasswordChecker()
+        {
+            // Load user names and passwords from file, populate Dictionary
+        }
 
         /// <summary>
         /// Checks the validity of a userName and password combination.
         /// </summary>
         /// <param name="userName"> The user name to be checked. </param>
-        /// <param name="password"> The password to be checked against. </param>
+        /// <param name="submittedPassword"> The password to be checked against. </param>
         /// <returns> A boolean representing whether or not the userName and password are a match against the records. </returns>
-        public bool Authenticate(string userName, string password)
+        public bool Authenticate(string userName, string submittedPassword)
         {
-            return true;
+            var usersPassword = this.validUsers[userName];
+            if (usersPassword == null)
+            {
+                return false;
+            }
+
+            return usersPassword == submittedPassword;
         }
     }
 }
