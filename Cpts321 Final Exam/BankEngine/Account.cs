89e06b97ddc3c32e095fa3bd3c70d9c1b1702d77
Arlo <arloe.jones3@gmail.com>
2021-12-10 18:19:28 -0800

    Implimented abstract parent class Account

--- Cpts321 Final Exam/BankEngine/Account.cs
+++ Cpts321 Final Exam/BankEngine/Account.cs
@@ -16,31 +16,118 @@ namespace Bank_Engine
     /// </summary>
     internal abstract class Account
     {
-        // Variables
-        // string AssociatedClientUserName?
-        // string accountID
-        // double balance
-        // LinkedList of transactions indicating history
-        // Static int number of Accounts created
+        /// <summary>
+        /// An ongoing count of the total number of accounts that have been instantiated.
+        /// </summary>
+        internal static int numberOfAccountsCreated;
 
-        // Withdraw function, Takes in an amount.
-        // If funds are sufficient, create withdraw transaction, add it to list, update balance
-        // else, return a bool indicating failure
+        /// <summary>
+        /// Contains a record of transactions on the account. retains up to 10 transactions.
+        /// </summary>
+        internal readonly Queue<Transaction> transactionHistory = new Queue<Transaction>();
 
-        // Deposit function
-        // create deposit transaction, add it to list, update balance
+        /// <summary>
+        /// Username of client who owns the account.
+        /// </summary>
+        public string associatedClientUserName;
+
+        /// <summary>
+        /// Unique account Identifier.
+        /// </summary>
+        public string accountID;
+
+        /// <summary>
+        /// Amount of money contained in the account.
+        /// </summary>
+        internal double balance = 0.00;
+
+        /// <summary>
+        /// Withdraws specified amount of money from the account.
+        /// </summary>
+        /// <param name="amount"> The amount of money to be withdrawn from the account. </param>
+        /// <returns> A bool indicating whether or not the withdraw was successful. </returns>
+        public bool Withdraw(double amount)
+        {
+            if (!(this.balance >= amount))
+            {
+                return false;
+            }
+
+            // Check size of Queue
+            if (this.transactionHistory.Count == 10)
+            {
+                this.transactionHistory.Dequeue();
+            }
+
+            this.transactionHistory.Enqueue(new Transaction(amount, "Withdraw")); // Create transaction and add to history
+            this.balance -= amount; // Update balance.
+            return true;
+        }
+
+        /// <summary>
+        /// Deposits specified amount of money into the account.
+        /// </summary>
+        /// <param name="amount"> The amount of money to be deposited into the account. </param>
+        /// <returns> A bool indicating whether or not the deposit was successful. </returns>
+        public bool Deposit(double amount)
+        {
+            // Check size of Queue
+            if (this.transactionHistory.Count == 10)
+            {
+                this.transactionHistory.Dequeue();
+            }
+
+            this.transactionHistory.Enqueue(new Transaction(amount, "Deposit")); // Create transaction and add to history
+            this.balance += amount; // Update balance.
+            return true;
+        }
 
         // Function to print history
         // Iterate through list of transactions, call print on each one.
 
         /// <summary>
+        /// Prints the transaction history of the account.
+        /// </summary>
+        /// <returns> Returns a list of strings, where each string indicates the kind of transaction that took place, and the amount involved. </returns>
+        public List<string> PrintHistory()
+        {
+            return this.transactionHistory.Select(transaction => transaction.PrintTransaction()).ToList();
+        }
+
+        /// <summary>
         /// Object that encapsulates the information of a transaction, used to record history of transactions.
         /// </summary>
-        private class Transaction
+        internal class Transaction
         {
-            // Amount
-            // associated accounts in the case of a transfer?
-            // Function to print transaction
+            /// <summary>
+            /// The amount of money involved in a transaction.
+            /// </summary>
+            private readonly double amount;
+
+            /// <summary>
+            /// The transaction type, either withdraw or deposit.
+            /// </summary>
+            private readonly string transactionType;
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="Transaction"/> class.
+            /// </summary>
+            /// <param name="amount"> The amount of money involved in the transaction.</param>
+            /// <param name="transactionType"> The kind of transaction, Withdraw or deposit. </param>
+            internal Transaction(double amount, string transactionType)
+            {
+                this.amount = amount;
+                this.transactionType = transactionType;
+            }
+
+            /// <summary>
+            /// Prints info contained in the transaction object.
+            /// </summary>
+            /// <returns> Returns a string containing the transaction type and amount. </returns>
+            internal string PrintTransaction()
+            {
+                return this.transactionType + " " + this.amount;
+            }
         }
     }
 }
