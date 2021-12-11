9410384e36916733d3e4894652e0d4e96fb4b44b
Arlo <arloe.jones3@gmail.com>
2021-12-10 20:40:29 -0800

    All add account tests pass.

--- Cpts321 Final Exam/Final Exam Functionality Testing Suite/BankEngineTests.cs
+++ Cpts321 Final Exam/Final Exam Functionality Testing Suite/BankEngineTests.cs
@@ -13,49 +13,6 @@ namespace Final_Exam_Functionality_Testing_Suite
     /// </summary>
     public class BankEngineTests
     {
-        [SetUp]
-        public void Setup()
-        {
-            // Declare Local variables
-            var bankEngine = new BankEngine();
-
-            // Initialize some history for the bank engine
-            bankEngine.AddUser("GiovanniMedici", "Administrator"); // Add an administrator
-
-            bankEngine.AddUser("NotARobot", "Client"); // Add a client
-            bankEngine.AddAccount("NotARobot", "Checking"); // Add a checking account
-            bankEngine.AccountTransaction("C00", "Deposit", 15000.00); // Initialize account with a deposit
-            bankEngine.AddAccount("NotARobot", "Saving"); // Add a savings account
-            bankEngine.AccountTransaction("S10", "Deposit", 15000.00); // Initialize account with a deposit
-            bankEngine.AddAccount("NotARobot", "Loan"); // Add a loan account
-            bankEngine.AccountTransaction("L20", "Withdraw", 50000.00); // Set Loan balance to the amount of the loan.
-
-            bankEngine.AddUser("StrugglingArtist", "Client"); // Add a client
-            bankEngine.AddAccount("StrugglingArtist", "Checking"); // Add a checking account
-            bankEngine.AccountTransaction("C31", "Deposit", 100.00);
-            bankEngine.AddAccount("StrugglingArtist", "Saving"); // Add a savings account
-            bankEngine.AccountTransaction("S41", "Deposit", 1000.00);
-            bankEngine.AddAccount("StrugglingArtist", "Loan"); // Add a loan account
-            bankEngine.AccountTransaction("L51", "Withdraw", 100000.00); // Set Loan balance to the amount of the loan.
-            bankEngine.AddAccount("StrugglingArtist", "Loan"); // Add a loan account
-            bankEngine.AccountTransaction("L52", "Withdraw", 50000.00); // Set Loan balance to the amount of the loan.
-
-            // Add transactions
-            bankEngine.AccountTransaction("C00", "L20", 5000.00); // NotARobot puts money towards loan
-            bankEngine.AccountTransaction("C00", "C31", 20.00); // NotARobot pays some money to struggling artist for a commissioned piece.
-            bankEngine.AccountTransaction("C31", "L51", 20.00); // StrugglingArtist puts money earned towards first loan.
-            bankEngine.AccountTransaction("C00", "Deposit", 10000.00); // NotARobot makes money mining Doge-coin.
-            bankEngine.AccountTransaction("C00", "Deposit", 10000.00); // NotARobot makes money mining Doge-coin.
-            bankEngine.AccountTransaction("C00", "Deposit", 10000.00); // NotARobot makes money mining Doge-coin.
-            bankEngine.AccountTransaction("C00", "Deposit", 10000.00); // NotARobot makes money mining Doge-coin.
-            bankEngine.AccountTransaction("C00", "Deposit", 10000.00); // NotARobot makes money mining Doge-coin.
-            bankEngine.AccountTransaction("C00", "Deposit", 10000.00); // NotARobot makes money mining Doge-coin.
-            bankEngine.AccountTransaction("C00", "Deposit", 10000.00); // NotARobot makes money mining Doge-coin.
-            bankEngine.AccountTransaction("C00", "Deposit", 10000.00); // NotARobot makes money mining Doge-coin.
-            bankEngine.AccountTransaction("C00", "Deposit", 10000.00); // NotARobot makes money mining Doge-coin.
-            bankEngine.AccountTransaction("C00", "L20", 45000.00); // NotARobot pays off loan
-        }
-
         /// <summary>
         /// Tests to see if BankEngine can add an administrator.
         /// </summary>
@@ -149,7 +106,7 @@ namespace Final_Exam_Functionality_Testing_Suite
             bankEngine.AddUser("NotARobot", "Client"); // Add a Client
 
             // Add checking account to user and see if the ID generated is what is expected.
-            Assert.AreEqual("C00", bankEngine.AddAccount("NotARobot", "Checking"));
+            Assert.AreEqual("C11", bankEngine.AddAccount("NotARobot", "Checking"));
         }
 
         /// <summary>
@@ -162,7 +119,7 @@ namespace Final_Exam_Functionality_Testing_Suite
             bankEngine.AddUser("NotARobot", "Client"); // Add a Client
 
             // Add checking account to user and see if the ID generated is what is expected.
-            Assert.AreEqual("S00", bankEngine.AddAccount("NotARobot", "Saving"));
+            Assert.AreEqual("S11", bankEngine.AddAccount("NotARobot", "Saving"));
         }
 
         /// <summary>
@@ -175,7 +132,7 @@ namespace Final_Exam_Functionality_Testing_Suite
             bankEngine.AddUser("NotARobot", "Client"); // Add a Client
 
             // Add checking account to user and see if the ID generated is what is expected.
-            Assert.AreEqual("L00", bankEngine.AddAccount("NotARobot", "Loan"));
+            Assert.AreEqual("L11", bankEngine.AddAccount("NotARobot", "Loan"));
         }
     }
 }
